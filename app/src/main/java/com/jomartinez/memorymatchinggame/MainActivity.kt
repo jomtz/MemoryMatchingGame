4f19f70acd4204f8fdc57d517eebe99726186b41
Jo Martinez <hello@jomartinez.com>
2024-03-14 00:02:16 -0400

    Add AlertDialog show

--- app/src/main/java/com/jomartinez/memorymatchinggame/MainActivity.kt
+++ app/src/main/java/com/jomartinez/memorymatchinggame/MainActivity.kt
@@ -5,10 +5,14 @@ import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -29,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvNumberPairs: TextView
 
     private lateinit var memoryGame: MemoryGame
-    private var boardSize: BoardSize = BoardSize.HARD
+    private var boardSize: BoardSize = BoardSize.MEDIUM
 
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,21 +60,80 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle item selection.
         return when (item.itemId) {
-            R.id.refresh -> {
-                Log.i(TAG, "Refresh, on Options Item Selected")
-                setupBoard()
+            R.id.mi_refresh -> {
+                if( memoryGame.getNumberMoves() > 0 && !memoryGame.haveWonGame() ){
+                    showAlertDialog(
+                        "Quit your current game?",
+                        null,
+                        View.OnClickListener {
+                            setupBoard()
+                        }
+                    )
+                } else {
+                    Log.i(TAG, "Refresh, on Options Item Selected")
+                    setupBoard()
+                }
                 true
             }
-            R.id.more -> {
-                Toast.makeText(this, "More", Toast.LENGTH_SHORT ).show()
+            R.id.mi_resize -> {
+//                Toast.makeText(this, "More", Toast.LENGTH_SHORT ).show()
+                showResizeDialog()
                 true
             }
             else -> return super.onOptionsItemSelected(item)
         }
     }
 
+    @SuppressLint("MissingInflatedId", "InflateParams")
+    private fun showResizeDialog() {
+        val boardSizeView = LayoutInflater.from(this).inflate(R.layout.dialog_board_size, null)
+        val radioGroupSize = boardSizeView.findViewById<RadioGroup>(R.id.radio_group_size)
+        when (boardSize) {
+            BoardSize.EASY -> radioGroupSize.check(R.id.radio_button_easy)
+            BoardSize.MEDIUM -> radioGroupSize.check(R.id.radio_button_medium)
+            BoardSize.HARD -> radioGroupSize.check(R.id.radio_button_hard)
+        }
+        showAlertDialog("Choose new size", boardSizeView, View.OnClickListener {
+            boardSize = when (radioGroupSize.checkedRadioButtonId) {
+                R.id.radio_button_easy -> BoardSize.EASY
+                R.id.radio_button_medium -> BoardSize.MEDIUM
+                else -> BoardSize.HARD
+            }
+            setupBoard()
+        })
+    }
+
+    private fun showAlertDialog(
+        title: String,
+        view: View?,
+        positiveClickListener: View.OnClickListener) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK"){_,_ ->
+                positiveClickListener.onClick(null)
+            }.show()
+    }
+
+    @SuppressLint("SetTextI18n")
     private fun setupBoard() {
         memoryGame = MemoryGame(boardSize)
+
+        when (boardSize) {
+            BoardSize.EASY -> {
+                tvNumberMoves.text = "Easy: 4 x 2"
+                tvNumberPairs.text = "Pairs: 0/4"
+            }
+            BoardSize.MEDIUM -> {
+                tvNumberMoves.text = "Medium: 6 x 3"
+                tvNumberPairs.text = "Pairs: 0/9"
+            }
+            BoardSize.HARD -> {
+                tvNumberMoves.text = "Hard: 6 x 4"
+                tvNumberPairs.text = "Pairs: 0/12"
+            }
+        }
 
         adapter = MemoryBoardAdapter(
             this,
